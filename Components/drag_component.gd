c6e538aec0eb3681b261e8a115025523583fdee8
Racer911-1 <douwe.bakker.03@gmail.com>
2026-08-22 11:48:19 +0200

    Add DropComponent Node. DropComponent Node gets added to "drop_target" group. DragComponent gets all DropComponents and calculates where to drop the parent node.

--- Components/drag_component.gd
+++ Components/drag_component.gd
@@ -4,6 +4,7 @@ extends Node
 enum State {
 	IDLE,
 	DRAGGING,
+	DROPPING,
 }
 
 @export var draggable: Control = null
@@ -24,6 +25,8 @@ func _process(delta: float) -> void:
 			pass
 		State.DRAGGING:
 			handle_dragging()
+		State.DROPPING:
+			handle_dropping()
 
 
 func _input(event: InputEvent) -> void:
@@ -32,7 +35,7 @@ func _input(event: InputEvent) -> void:
 		drag_offset = get_offset()
 		change_state(State.DRAGGING)
 	if event.is_action_released(&"mouse_button_left"):
-		change_state(State.IDLE)
+		change_state(State.DROPPING)
 
 
 func change_state(new_state: State) -> void:
@@ -44,6 +47,12 @@ func handle_dragging() -> void:
 	draggable.position = mouse_pos + drag_offset
 
 
+func handle_dropping() -> void:
+	var target = find_drop_target()
+	if target:
+		draggable.position = target.position + target.size / 2 - draggable.size / 2
+	change_state(State.IDLE)
+
 func get_offset() -> Vector2:
 	var mouse_pos = get_viewport().get_mouse_position()
 	var parent_pos = draggable.position
@@ -53,3 +62,22 @@ func get_offset() -> Vector2:
 
 func has_point(point: Vector2) -> bool:
 	return Rect2(draggable.position, draggable.size).has_point(point)
+
+
+func find_drop_target() -> Control:
+	var my_rect := draggable.get_global_rect()
+	var best_target: Control = null
+	var best_overlap: float = 0.0
+
+	for drop_component in get_tree().get_nodes_in_group("drop_target"):
+		var target := (drop_component as DropComponent).droppable
+		if target == null:
+			continue
+		var target_rect := target.get_global_rect()
+		if target_rect.intersects(my_rect):
+			var overlap := my_rect.intersection(target_rect).get_area()
+			if overlap > best_overlap:
+				best_overlap = overlap
+				best_target = target
+
+	return best_target
