44ac2200710de55b53362794b8b2a0418da2c08d
Racer911-1 <douwe.bakker.03@gmail.com>
2026-08-24 16:05:58 +0200

    WIP Occupancy state

--- Components/drop_component.gd
+++ Components/drop_component.gd
@@ -1,12 +1,27 @@
 class_name DropComponent
 extends Node
 
+signal occupancy_changed(state: State)
+
+enum State {
+	UNOCCUPIED,
+	OCCUPIED,
+}
+
 @export var droppable: Control
+
+var state := State.UNOCCUPIED
 
 func _ready() -> void:
 	add_to_group("drop_target")
-	var parent = get_parent() as Control
+	var parent := get_parent() as Control
 	if parent:
 		droppable = parent
 	else:
+		# TODO: proper error handling
 		return
+	droppable.child_entered_tree.connect(_on_child_entered_tree)
+
+
+func _on_child_entered_tree(node: Node) -> void:
+	print(node.find_child("DropComponent"))
